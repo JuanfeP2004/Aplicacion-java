0d1ebea17fbb6080118c2680c2f7cf1216258b85
JuanfeP2004 <juanf.ramirezc@upb.edu.co>
2022-03-30 21:45:08 -0500

    commit 1

---
+++ Aplicacion_entregable.java
@@ -0,0 +1,18 @@
+public class Aplicacion_entregable {
+	public static void main(String[] args) {
+		
+		System.out.println(" __  __ _               _ _                _             ");
+		System.out.println("|  \\/  (_)             | (_)              (_)            ");
+		System.out.println("| \\  / |_    __ _ _ __ | |_  ___ __ _  ___ _  ___  _ __  ");
+		System.out.println("| |\\/| | |  / _` | '_ \\| | |/ __/ _` |/ __| |/ _ \\| '_ \\ ");
+		System.out.println("| |  | | | | (_| | |_) | | | (_| (_| | (__| | (_) | | | |");
+		System.out.println("|_|  |_|_|  \\__,_| .__/|_|_|\\___\\__,_|\\___|_|\\___/|_| |_|");
+		System.out.println("                 | |                                     ");
+		System.out.println("                 |_|                                     ");
+		System.out.println("");
+		System.out.println(">>>>Z>o>d>i>a>c>o>>>>");
+		System.out.println(">>>>D>i>a>s> >v>i>v>i>d>o>s>>>>");
+		System.out.println(">>>>A>n>o>s> >p>e>r>r>o>>>>");
+		System.out.println(">>>>N>u>m>e>r>o>l>o>g>i>a>>>>");
+	}
+}
